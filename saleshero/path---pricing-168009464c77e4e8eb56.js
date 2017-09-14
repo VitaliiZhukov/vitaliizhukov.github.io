0fc9afe6e691cae7171abddae11d52890ea59227
Vitalii <zhukov-vit@yandex.ru>
2017-09-14 20:25:29 +0300

    added sh lp

---
+++ saleshero/path---pricing-168009464c77e4e8eb56.js
@@ -0,0 +1,2 @@
+webpackJsonp([93449047059328],{"./node_modules/json-loader/index.js!./.cache/json/pricing.json":function(n,e){n.exports={data:{markdownRemark:{frontmatter:{title:"Pricing"},html:"<p>This is terms page.</p>"}},pathContext:{slug:"/pricing/"}}}});
+//# sourceMappingURL=path---pricing-168009464c77e4e8eb56.js.map
