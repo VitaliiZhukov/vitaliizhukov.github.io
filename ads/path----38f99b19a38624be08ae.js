d1aaa819c954aaa60e4b5d38e565800536aaccb1
Vitalii <zhukov-vit@yandex.ru>
2017-09-22 12:29:57 +0300

    Website in progress

--- ads/path----38f99b19a38624be08ae.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([60335399758886],{"./node_modules/json-loader/index.js!./.cache/json/layout-index.json":function(e,l){e.exports={data:{allMarkdownRemark:{edges:[{node:{frontmatter:{scope:null,title:"Privacy policy"},fields:{slug:"/privacy/"}}},{node:{frontmatter:{scope:null,title:"Home Page"},fields:{slug:"/"}}},{node:{frontmatter:{scope:["HEADER","FOOTER"],title:"Saleshero Skills"},fields:{slug:"/skills/"}}},{node:{frontmatter:{scope:null,title:"Terms of Service"},fields:{slug:"/terms/"}}}]}},layoutContext:{}}}});
-//# sourceMappingURL=path----38f99b19a38624be08ae.js.map
