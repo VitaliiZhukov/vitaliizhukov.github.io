dc23fea407b6f13e2507c353045aa702fa140255
Vitalii <zhukov-vit@yandex.ru>
2017-10-12 11:40:50 +0300

    Modified layouts

---
+++ saleshero/path---privacy-351bf52400d0cedb50e1.js
@@ -0,0 +1,2 @@
+webpackJsonp([0x97d0984cce63],{"./node_modules/json-loader/index.js!./.cache/json/privacy.json":function(e,o){e.exports={data:{markdownRemark:{frontmatter:{title:"Privacy policy",items:null},html:"<p>Terms of service go here!</p>"}},pathContext:{slug:"/privacy/"}}}});
+//# sourceMappingURL=path---privacy-351bf52400d0cedb50e1.js.map
