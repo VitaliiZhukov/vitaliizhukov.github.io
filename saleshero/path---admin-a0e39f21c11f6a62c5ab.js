60c6797c390033987ce7ec4adea0c98fba6ce0df
Vitalii <zhukov-vit@yandex.ru>
2017-10-12 09:51:39 +0300

    Modified layouts

--- saleshero/path---admin-a0e39f21c11f6a62c5ab.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([0xc8367055b07b],{"./node_modules/json-loader/index.js!./.cache/json/admin.json":function(n,o){n.exports={pathContext:{}}}});
-//# sourceMappingURL=path---admin-a0e39f21c11f6a62c5ab.js.map
