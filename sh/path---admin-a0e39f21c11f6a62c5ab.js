7ef3fb80116913275153c37950fe643c79af3b02
Vitalii <zhukov-vit@yandex.ru>
2017-10-27 09:43:02 +0300

    Modified layouts

--- sh/path---admin-a0e39f21c11f6a62c5ab.js
+++ sh/path---admin-a0e39f21c11f6a62c5ab.js
@@ -1,2 +1,2 @@
-webpackJsonp([0xc8367055b07b],{408:function(t,n){t.exports={pathContext:{}}}});
+webpackJsonp([0xc8367055b07b],{412:function(t,n){t.exports={pathContext:{}}}});
 //# sourceMappingURL=path---admin-a0e39f21c11f6a62c5ab.js.map
